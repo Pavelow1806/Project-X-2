65ca9c2a036ecfa4f99edfafaa0bf9c991776ba9
Pavelow <jamescoyle2@hotmail.com>
2020-02-01 21:08:33 +0000

    sean push

--- Login-Server/Login-Server/ProcessPacket.cs
+++ Login-Server/Login-Server/ProcessPacket.cs
@@ -12,36 +12,36 @@ namespace Login_Server
         public static void Process(object sender, PacketEventArgs e)
         {
             Log.Write(LogType.Information, $"Starting to process packet:\n{e.Packet.ToString()}");
-            switch (e.Packet.Source)
-            {
-                case ConnectionType.UNKNOWN:
-                    Log.Write(LogType.Error, $"The source type was invalid");
-                    return;
-                case ConnectionType.GAMESERVER:
-                    break;
-                case ConnectionType.CLIENT:
-                    break;
-                case ConnectionType.LOGINSERVER:
-                    Log.Write(LogType.Error, $"The source type was invalid");
+            //switch (e.Packet.Source)
+            //{
+            //    case ConnectionType.UNKNOWN:
+            //        Log.Write(LogType.Error, $"The source type was invalid");
+            //        return;
+            //    case ConnectionType.GAMESERVER:
+            //        break;
+            //    case ConnectionType.CLIENT:
+            //        break;
+            //    case ConnectionType.LOGINSERVER:
+            //        Log.Write(LogType.Error, $"The source type was invalid");
 
-                    return;
-                case ConnectionType.SYNCSERVER:
-                    break;
-                case ConnectionType.TOOL:
-                    Process_ToolPacket(e.Packet, (Server)e.Source);
-                    break;
-                default:
-                    Log.Write(LogType.Error, $"The source type was invalid");
-                    return;
-            }
-            Log.Write(LogType.Information, $"Finished processing packet:\n{e.Packet.ToString()}");
+            //        return;
+            //    case ConnectionType.SYNCSERVER:
+            //        break;
+            //    case ConnectionType.TOOL:
+            //        Process_ToolPacket(e.Packet, (Server)e.Source);
+            //        break;
+            //    default:
+            //        Log.Write(LogType.Error, $"The source type was invalid");
+            //        return;
+            //}
+            //Log.Write(LogType.Information, $"Finished processing packet:\n{e.Packet.ToString()}");
         }
         public static void Process_ToolPacket(Packet packet, Server Destination)
         {
             switch ((ToolSendPacketNumbers)packet.PacketNumber)
             {
                 case ToolSendPacketNumbers.RequestLogs:
-                    SendData.SendLogs(packet.Destination, Destination);
+                    //SendData.SendLogs(packet.Destination, Destination);
                     break;
                 default:
                     break;
