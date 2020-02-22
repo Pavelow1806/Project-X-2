f4b57c286b3e2e99d0556763cf5b73a3e37f2ded
seank55 <seankhanna@outlook.com>
2020-02-22 19:08:11 +0000

    Created a simple quest outline and reward outline.

---
+++ Client/Test Environment/Assets/Scripts/QuestDetails.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RewardList = System.Collections.Generic.Dictionary<int, int>;
+
+public class QuestDetails : MonoBehaviour
+{
+    //-----------------------------------------------
+    // Public Variables
+    //-----------------------------------------------
+
+    // Manager to get information about the rewards
+    public GameObject m_rewardManager;
+
+    //-----------------------------------------------
+    // Private Variables
+    //-----------------------------------------------
+
+    // The quest ID 
+    private int m_questId = -1;
+
+    // The quest description
+    private string m_questDescription = "";
+
+    // Dictionary of reward item Id and quantity
+    private RewardList m_rewards;
+
+    //-----------------------------------------------
+    // Public Methods
+    //-----------------------------------------------
+
+    public void Init(int questId)
+    {
+        // Initialise questId
+        m_questId = questId;
+
+        // Gather information about quest
+        PopulateQuest();
+
+        // Get the Reward Manager
+        RewardManager managerScript = m_rewardManager.GetComponent<RewardManager>();
+
+        // Initialise information 
+        managerScript.Init(m_questId, RewardManager.RewardSource.Quest);
+
+        // Get the reward list from the manager
+        m_rewards = managerScript.GetRewardList();
+    }
+
+    public RewardList GetRewardList()
+    {
+        return m_rewards;
+    }
+
+    public void PopulateQuest()
+    {
+
+    }
+
+    public string GetQuestDescription()
+    {
+        return m_questDescription;
+    }
+}
---
+++ Client/Test Environment/Assets/Scripts/RewardManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RewardList = System.Collections.Generic.Dictionary<int, int>;
+
+public class RewardManager : MonoBehaviour
+{
+    //-----------------------------------------------
+    // Public Variables
+    //-----------------------------------------------
+
+    // Where is the request for a reward coming from
+    public enum RewardSource
+    {
+        Quest,
+    };
+
+    //-----------------------------------------------
+    // Private Variables
+    //-----------------------------------------------
+
+    // Dictionary of reward item Id and quantity
+    private RewardList m_rewards = new RewardList();
+
+    // If reward comes from quest use quest Id
+    private int m_questId = -1;
+
+    //-----------------------------------------------
+    // Public Methods
+    //-----------------------------------------------
+
+    public void Init(int sourceId, RewardSource sourceType) 
+    {
+        switch (sourceType)
+        {
+            case RewardSource.Quest:
+                m_questId = sourceId;
+                FillRewardList(m_questId);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public RewardList GetRewardList()
+    {
+        return m_rewards;
+    }
+
+
+    //-----------------------------------------------
+    // Private Methods
+    //-----------------------------------------------
+
+    private void FillRewardList(int id)
+    {
+        if (id == 10)
+        {
+            m_rewards.Add(1, 200);
+        }
+    }
+}
